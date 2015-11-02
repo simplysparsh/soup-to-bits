1a57365acce35ff7a711c06a5962ad947316fc84
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-11-02 00:38:00 -0500

    Add before_action, render in json and xml

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,9 +1,31 @@
 class SoupsController < ApplicationController
+  before_action :fetch_soup, only: [:show, :edit, :update, :destroy]
+
   def index
     @soups = Soup.all
+
+    respond_to do |format|
+      format.html #render index.html.erb
+      format.json {render json: @soups}
+      format.xml {render xml: @soups}
+    end
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def fetch_soup
     @soup = Soup.find(params[:id])
   end
 end
