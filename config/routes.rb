732a7493128e1a9f85c08b78d4b8f30501e1ecfd
Sparsh Saxena <simplysparsh.s@gmail.com>
2015-11-04 11:29:34 -0500

    Add route for toggle_featured

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :soups, :categories
 
   root to: 'categories#index'
+  get '/allcategories', to: redirect('/')
+
+  get '/soup/:id/toggle_featured', to: 'soups#toggle_featured', as: 'toggle_featured'
+
 end
